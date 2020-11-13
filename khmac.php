1ffea2671dd6df2aee9adc7b8e9d86d3bc0d65c2
Mahtat <yassinmhtat@gmail.com>
2020-11-13 22:39:15 +0100

    Add the `khmac.php` file to the project

---
+++ khmac.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This module is part of the project PHPhmac.
+ *
+ * This module is an PHP implementation of the HMAC algorithm described by the
+ * standard <<Key hash message authentication code (HMAC) (FIPS PUB 198)>>.
+ */
+
+
+?>
