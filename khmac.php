9aa729b3445b69bfd80dbf39229e3dc972522856
Mahtat <yassinmhtat@gmail.com>
2020-11-14 16:21:23 +0100

    Create the three important variables $ascii, $ipad and $ipad.
    $ipad and $opad were chosen in order to have an important Hamming distance.

--- khmac.php
+++ khmac.php
@@ -7,4 +7,22 @@
  */
 
 
+// All ASCII characters from 0 to 255
+$ascii = null;
+for ($code=0; $code < 256; $code++) {
+    $ascii .= chr($code);
+}
+
+// $ipad and $opad were chosen in order to have an important Hamming distance.
+$opad = null;
+for ($code=0; $code < 256; $code++) {
+    $opad .= chr($code ^ 0x5C);
+}
+
+$ipad = null;
+for ($code=0; $code < 256; $code++) {
+    $ipad .= chr($code ^ 0x36);
+}
+
+
 ?>
